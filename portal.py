1ed414acb139915df7144d648ff19ce957e9982d
HCLivess <admin@bismuth.cz>
2020-04-05 18:48:35 +0200

    portal and hnmon repos

---
+++ portal.py
@@ -0,0 +1,17 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("portal.html")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": "static"}),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(80)
+    tornado.ioloop.IOLoop.current().start()
